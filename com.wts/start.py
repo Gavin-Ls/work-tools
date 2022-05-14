cb965579e30434089cf93799b802b6a0b630064c
刘赛 <liusai>
2022-05-14 23:25:05 +0800

    chore(change path):
    调整目录结构

--- com.wts/start.py
+++ com.wts/start.py
@@ -1,13 +1,14 @@
+# coding=utf-8
 import sys
 
 from PySide6.QtWidgets import QApplication
 
 sys.path.append('cloud')
-from cloud.StartMenu import StartMainMenu
 
 
 class MainStart:
     def __init__(self):
+        from cloud.StartMenu import StartMainMenu
         self.start_main_menu = StartMainMenu(app)
         self.start_main_menu.show()
         self.start_main_menu.to_macos()
