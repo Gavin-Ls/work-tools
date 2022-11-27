5dc1220b9c905e65b6f90a39ce1a07144298301b
刘赛 <liusai>
2022-11-27 11:57:40 +0800

    refactor(重新调整项目目录):
    重新调整项目目录

--- com/wts/cloud/start.py
+++ com/wts/cloud/start.py
@@ -3,15 +3,15 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
-sys.path.append('cloud')
-sys.path.append('cloud/business')
-sys.path.append('cloud/ui')
-sys.path.append('cloud/utils')
+sys.path.append('')
+sys.path.append('business')
+sys.path.append('ui')
+sys.path.append('utils')
 
 
 class MainStart:
     def __init__(self):
-        from cloud.business.StartMenu import StartMainMenu
+        from business.StartMenu import StartMainMenu
         self.start_main_menu = StartMainMenu(app)
         self.start_main_menu.show()
         self.start_main_menu.change_to_windows_vista()
